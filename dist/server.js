7be45aba2684b3c16fd729bd935bb8e50c791c7b
Nathalia <nathalia.gramst@gmail.com>
2022-11-09 19:37:49 -0300

    Cleaning cache for gitignore

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-var server = express();
-server.get("/health", function (req, res) {
-    res.send('ok');
-});
-server.listen(4000, function () {
-    console.log("Magic happens on port 4000!");
-});
